96744ae6a2a01dd9265ae5e89d7751b6f7168288
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2025-01-07 17:41:44 +0800

    pretty much release

--- common/src/main/java/net/anawesomguy/breakingbedrock/BreakingBedrock.java
+++ common/src/main/java/net/anawesomguy/breakingbedrock/BreakingBedrock.java
@@ -18,60 +18,87 @@ public final class BreakingBedrock {
     public static final float DESTROY_TIME;
     public static final float EXPLOSION_RESIST;
     public static final int MINING_TIER;
+    public static final boolean DROP_BEDROCK;
 
     static {
-        LOGGER.info("Initializing Breaking Bedrock. This mod overwrites bedrock, and may break with other mods that do the same!");
+        Logger log = LOGGER;
+        log.info("Initializing Breaking Bedrock. This mod overwrites bedrock, and may break with other mods that do the same!");
         Properties properties = new Properties();
         float destroyTime, explosionResist;
         int miningLevel;
+        boolean dropBedrock;
         File configFile = configDir().resolve(BreakingBedrock.MOD_ID + ".properties").toFile();
         try {
             Properties temp = new Properties();
             temp.load(new FileInputStream(configFile));
             String time = temp.getProperty("destroy_time"),
                    resist = temp.getProperty("explosion_resist"),
-                   level = temp.getProperty("mining_level");
-            if (time == null || !((destroyTime = Float.parseFloat(time)) > -1) || destroyTime == Float.POSITIVE_INFINITY) {
+                   level = temp.getProperty("mining_level"),
+                   drop = temp.getProperty("drop_bedrock");
+
+            if (time != null && ((destroyTime = Float.parseFloat(time)) >= 0 || destroyTime == -1F))
+                properties.setProperty("destroy_time", time);
+            else {
                 properties.setProperty("destroy_time", "100");
                 destroyTime = 100F;
-                LOGGER.debug("Correcting invalid config value {}!", time);
-            } else properties.setProperty("destroy_time", time);
-            if (resist == null || !((explosionResist = Float.parseFloat(resist)) > 0) || explosionResist == Float.POSITIVE_INFINITY) {
+                log.debug("Correcting invalid config value {}!", time);
+            }
+
+            if (resist != null && (explosionResist = Float.parseFloat(resist)) >= 0)
+                properties.setProperty("explosion_resist", resist);
+            else {
                 properties.setProperty("explosion_resist", "3600000");
                 explosionResist = 3600000F;
-                LOGGER.debug("Correcting invalid config value {}!", resist);
-            } else properties.setProperty("explosion_resist", resist);
-            if (level == null || !((miningLevel = Integer.parseInt(level)) > 0)) {
+                log.debug("Correcting invalid config value {}!", resist);
+            }
+
+            if (level != null && (miningLevel = Integer.parseInt(level)) >= 0) {
+                properties.setProperty("mining_level", level);
+            } else {
                 properties.setProperty("mining_level", "4");
                 miningLevel = 4;
-                LOGGER.debug("Correcting invalid config value {}!", level);
-            } else properties.setProperty("mining_level", level);
+                log.debug("Correcting invalid config value {}!", level);
+            }
+
+            boolean isFalse = "false".equalsIgnoreCase(drop);
+            boolean isTrue = "true".equalsIgnoreCase(drop);
+            if (drop == null || (!isFalse && !isTrue)) { // if value is not set or is neither "false" nor "true"
+                properties.setProperty("drop_bedrock", "false");
+                dropBedrock = false;
+                log.debug("Correcting invalid config value {}!", drop);
+            } else
+                properties.setProperty("drop_bedrock", String.valueOf(dropBedrock = isTrue));
         } catch (IOException | IllegalArgumentException e) {
-            LOGGER.info("Couldn't read config file (likely corrupted or missing)! Attempting to (re)create it.");
+            log.info("Couldn't read config file (likely corrupted or missing)! Attempting to (re)create it.");
             properties.setProperty("destroy_time", "100");
             properties.setProperty("explosion_resist", "3600000");
             properties.setProperty("mining_level", "4");
+            properties.setProperty("drop_bedrock", "false");
             destroyTime = 100F;
             explosionResist = 3600000F;
             miningLevel = 4;
+            dropBedrock = false;
         }
 
         EXPLOSION_RESIST = explosionResist;
         DESTROY_TIME = destroyTime;
         MINING_TIER = miningLevel;
+        DROP_BEDROCK = dropBedrock;
 
         try {
             properties.store(new FileOutputStream(configFile),
-                  """
-                  destroy_time: The destroy time for bedrock. (obsidian is 50, stone is 1.5, -1 is indestructible)
-                  explosion_resist: The explosion resistance for bedrock. (stone is 6, glass is 0.3)
-                  mining_level: The mining level required to mine bedrock. (netherite is 4)
-                 """);
+                             """
+                                  destroy_time: The destroy time for bedrock. (obsidian is 50, stone is 1.5, -1 is indestructible)
+                                  explosion_resist: The explosion resistance for bedrock. (stone is 6, glass is 0.3)
+                                  mining_level: The mining level required to mine bedrock. (netherite is 4)
+                                  drop_bedrock: Whether bedrock should drop as a block when broken.
+                                 """);
         } catch (IOException e) {
             LOGGER.error("Unable to create/modify config file!", e);
         }
 
-        LOGGER.debug("Config initialized with values: destroyTime={}, explosionResist={}, miningLevel={}", destroyTime, explosionResist, miningLevel);
+        LOGGER.debug("Config initialized with values: destroyTime={}, explosionResist={}, miningLevel={}, drop_bedrock={}",
+                     destroyTime, explosionResist, miningLevel, dropBedrock);
     }
 
     @ExpectPlatform
--- common/src/main/java/net/anawesomguy/breakingbedrock/mixin/BlocksMixin_ReplaceBedrock.java
+++ common/src/main/java/net/anawesomguy/breakingbedrock/mixin/BlocksMixin_ReplaceBedrock.java
@@ -11,9 +11,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(Blocks.class)
-public abstract class BlocksMixin {
+public abstract class BlocksMixin_ReplaceBedrock {
     @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "net/minecraft/world/level/block/Block", ordinal = 0), slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=bedrock")))
     private static Block breakingbedrock$replaceBedrock(Properties properties) {
-        return new BedrockBlock(properties.strength(BreakingBedrock.DESTROY_TIME, BreakingBedrock.EXPLOSION_RESIST));
+        return new BedrockBlock(properties.strength(BreakingBedrock.DESTROY_TIME, BreakingBedrock.EXPLOSION_RESIST).requiresCorrectToolForDrops());
     }
 }
